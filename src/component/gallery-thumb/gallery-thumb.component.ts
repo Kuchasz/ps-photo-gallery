6908b1be4d3a495e206b01e2ba44a191ccad7f53
Rafal Pysz <kuchasz1@o2.pl>
2017-10-23 22:52:00 +0200

    fixed scroll issue

--- src/component/gallery-thumb/gallery-thumb.component.ts
+++ src/component/gallery-thumb/gallery-thumb.component.ts
@@ -1,123 +1,122 @@
 import {
-  Component,
-  Input,
-  ChangeDetectionStrategy,
-  ElementRef,
-  Renderer2,
-  OnInit
+    Component,
+    Input,
+    ChangeDetectionStrategy,
+    ElementRef,
+    Renderer2,
+    OnInit
 } from "@angular/core";
-import { GalleryService } from "../../service/gallery.service";
-import { GalleryState } from "../../service/gallery.state";
-import { GalleryThumbConfig } from "../../config";
+import {GalleryService} from "../../service/gallery.service";
+import {GalleryState} from "../../service/gallery.state";
+import {GalleryThumbConfig} from "../../config";
 import * as Hammer from "hammerjs";
-import { TweenLite } from "gsap";
+import {TweenLite} from "gsap";
 
 @Component({
-  selector: "gallery-thumb",
-  templateUrl: "./gallery-thumb.component.html",
-  styleUrls: ["./gallery-thumb.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: "gallery-thumb",
+    templateUrl: "./gallery-thumb.component.html",
+    styleUrls: ["./gallery-thumb.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GalleryThumbComponent implements OnInit {
-  @Input() state: GalleryState;
-  @Input() config: GalleryThumbConfig;
+    @Input() state: GalleryState;
+    @Input() config: GalleryThumbConfig;
 
-  elContainer: any;
-  thumbsDelta: number = 0;
+    elContainer: any;
+    thumbsDelta: number = 0;
 
-  constructor(
-    public gallery: GalleryService,
-    private el: ElementRef,
-    private renderer: Renderer2
-  ) {}
-
-  ngOnInit() {
-    window.addEventListener("keydown", e => {
-      if (e.keyCode === 37) this.gallery.prev();
-      if (e.keyCode === 39) this.gallery.next();
-    });
-
-    if (this.gallery.config.gestures) {
-      this.elContainer = this.el.nativeElement.querySelector(
-        ".g-thumb-container"
-      );
-
-      const mc = new Hammer(this.el.nativeElement);
-
-      TweenLite.set(this.elContainer, { x: -this.config.width / 2 });
-
-      mc.on("panend", e => {
-        if (Math.abs(e.velocityX) < 0.5) {
-          this.thumbsDelta += e.deltaX;
-          return;
-        }
-
-        const targetDelta = e.deltaX * Math.abs(e.velocityX);
-
-        this.thumbsDelta += targetDelta;
-
-        this.thumbsDelta = this._valBetween(
-          this.thumbsDelta,
-          this.getMaxDelta(),// + this.config.width / 2,
-          -this.config.width / 2
-        );
-
-        TweenLite.killTweensOf(this.elContainer);
-        TweenLite.to(this.elContainer, 1, { x: this.thumbsDelta });
-      });
-
-      mc.on("pan", e => {
-        let targetDelta = this.thumbsDelta + e.deltaX;
-
-        targetDelta =
-          targetDelta > -this.config.width / 2
-            ? -this.config.width / 2
-            : targetDelta < this.getMaxDelta()
-              ? this.getMaxDelta()
-              : targetDelta;
-
-        TweenLite.killTweensOf(this.elContainer);
-        TweenLite.to(this.elContainer, 0.25, { x: targetDelta });
-      });
+    constructor(public gallery: GalleryService,
+                private el: ElementRef,
+                private renderer: Renderer2) {
     }
 
-    this.gallery.state.subscribe(
-      x => x.currIndex !== undefined && this._scrollImages(x.currIndex)
-    );
+    ngOnInit() {
+        window.addEventListener("keydown", e => {
+            if (e.keyCode === 37) this.gallery.prev();
+            if (e.keyCode === 39) this.gallery.next();
+        });
 
-  }
+        if (this.gallery.config.gestures) {
+            this.elContainer = this.el.nativeElement.querySelector(
+                ".g-thumb-container"
+            );
 
-  private _valBetween(v, min, max) {
-    return Math.min(max, Math.max(min, v));
-  }
+            const hammer = new Hammer(this.elContainer);
 
-  selectImage(index: number) {
-    this.gallery.selectImage(index);
-  }
+            TweenLite.set(this.elContainer, {x: -this.config.width / 2});
 
-  _scrollImages(index) {
-    const x =
-      index /
-        this.getImages().length *
-        (this.getMaxDelta() - this.config.width / 2) -
-      this.config.width / 2;
-    this.thumbsDelta = x;
-    TweenLite.killTweensOf(this.elContainer);
-    TweenLite.to(this.elContainer, 1, { x: this.thumbsDelta });
-  }
+            hammer.on("panend", e => {
+                if (Math.abs(e.velocityX) < 0.5) {
+                    this.thumbsDelta += e.deltaX;
+                    return;
+                }
 
-  getMaxDelta() {
-    return -(
-      this.getImages().length * this.config.width -
-      this.config.width / 2
-    );
-  }
+                const targetDelta = e.deltaX * Math.abs(e.velocityX);
 
-  getImages() {
-    return this.state.directories[this.state.currDirectory].images;
-  }
+                this.thumbsDelta += targetDelta;
 
-  getThumbImage(i: number) {
-    return `url(${this.getImages()[i].thumbnail || this.getImages()[i].src})`;
-  }
+                this.thumbsDelta = this._valBetween(
+                    this.thumbsDelta,
+                    this.getMaxDelta(),// + this.config.width / 2,
+                    -this.config.width / 2
+                );
+
+                TweenLite.to(this.elContainer, 1, {x: this.thumbsDelta});
+            });
+
+            hammer.on("pan", e => {
+                let targetDelta = this.thumbsDelta + e.deltaX;
+
+                targetDelta =
+                    targetDelta > -this.config.width / 2
+                        ? -this.config.width / 2
+                        : targetDelta < this.getMaxDelta()
+                        ? this.getMaxDelta()
+                        : targetDelta;
+
+                TweenLite.to(this.elContainer, 0.25, {x: targetDelta});
+            });
+
+            hammer.on('panstart', () => console.log('pan-start'));
+
+        }
+
+        this.gallery.state.subscribe(
+            x => x.currIndex !== undefined && this._scrollImages(x.currIndex)
+        );
+
+    }
+
+    private _valBetween(v, min, max) {
+        return Math.min(max, Math.max(min, v));
+    }
+
+    selectImage(index: number) {
+        this.gallery.selectImage(index);
+    }
+
+    _scrollImages(index) {
+        const x =
+            index /
+            this.getImages().length *
+            (this.getMaxDelta() - this.config.width / 2) -
+            this.config.width / 2;
+        this.thumbsDelta = x;
+        TweenLite.to(this.elContainer, 1, {x: this.thumbsDelta});
+    }
+
+    getMaxDelta() {
+        return -(
+            this.getImages().length * this.config.width -
+            this.config.width / 2
+        );
+    }
+
+    getImages() {
+        return this.state.directories[this.state.currDirectory].images;
+    }
+
+    getThumbImage(i: number) {
+        return `url(${this.getImages()[i].thumbnail || this.getImages()[i].src})`;
+    }
 }
--- src/demo/app.module.ts
+++ src/demo/app.module.ts
@@ -1,46 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './component/app.component';
+import {AppComponent} from './component/app.component';
 import {GalleryModule} from "../gallery.module";
 import {checkIfMobile} from "../utils/browser";
 import {DisplayModes} from "../config/gallery.config";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    GalleryModule.forRoot(
-      {
-        style: {
-          background: "#333",
-          width: "100vw",
-          height: "100vh"
-        },
-        description: {
-          position: 'bottom',
-          overlay: false,
-          text: true,
-          counter: true
-        },
-        thumbnails: {
-          width: 100,
-          height: 100,
-          position: 'bottom',
-          space: 20
-        },
-        bullets: undefined,
-        navigation: {},
-        gestures: true,
-        displayMode: checkIfMobile() ? DisplayModes.Compact : DisplayModes.Full
-      }
-    )
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        GalleryModule.forRoot(
+            {
+                style: {
+                    background: "#333",
+                    width: "100vw",
+                    height: "100vh"
+                },
+                description: {
+                    position: 'bottom',
+                    overlay: false,
+                    text: true,
+                    counter: true
+                },
+                thumbnails: {
+                    width: 100,
+                    height: 100,
+                    position: 'bottom',
+                    space: 20
+                },
+                bullets: undefined,
+                navigation: {},
+                gestures: true,
+                displayMode: checkIfMobile() ? DisplayModes.Compact : DisplayModes.Full
+            }
+        )
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
