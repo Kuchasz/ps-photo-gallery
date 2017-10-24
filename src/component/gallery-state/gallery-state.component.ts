72e5a53aac54a80bc0086b83d418284dad2fa482
Rafal Pysz <kuchasz1@o2.pl>
2017-10-24 23:54:39 +0200

    fullscreen mode added, ui tweaks with icons

--- src/component/gallery-state/gallery-state.component.ts
+++ src/component/gallery-state/gallery-state.component.ts
@@ -1,7 +1,8 @@
-import { Component, ChangeDetectionStrategy, Input } from "@angular/core";
-import { GalleryService } from "../../service/gallery.service";
-import { GalleryState } from "../../service/gallery.state";
-import { GalleryConfig } from "../../index";
+import {Component, ChangeDetectionStrategy, Input} from "@angular/core";
+import {GalleryService} from "../../service/gallery.service";
+import {GalleryState} from "../../service/gallery.state";
+import {GalleryConfig} from "../../index";
+import * as screenfull from "screenfull";
 
 @Component({
     selector: 'gallery-state',
@@ -9,23 +10,34 @@ import { GalleryConfig } from "../../index";
     styleUrls: ['./gallery-state.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GalleryStateComponent{
+export class GalleryStateComponent {
     @Input() state: GalleryState;
     @Input() config: GalleryConfig;
 
-    constructor(public gallery: GalleryService){
-        
+    constructor(public gallery: GalleryService) {
+
     }
 
-    get images(){
+    toggleFullscreen() {
+        if (screenfull.isFullscreen)
+            screenfull.exit();
+        else if (screenfull.enabled)
+            screenfull.request();
+    }
+
+    get fullscreenEnabled(){
+        return screenfull.enabled;
+    }
+
+    get images() {
         return this.currentDirectory.images;
     }
 
-    get currentImage(){
+    get currentImage() {
         return this.images[this.state.currIndex];
     }
 
-    get currentDirectory(){
+    get currentDirectory() {
         return this.state.directories[this.state.currDirectory];
     }
 }
