80d605455db684d8049e3f82540af79ae911f6fc
Kuchasz <kuchasz1@o2.pl>
2020-08-17 17:43:21 +0200

    changes in directories list

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,3 +1,3 @@
-export const clamp = (min: number, max: number) => (value: number) => {
+const clamp = (min: number, max: number) => (value: number) => {
     return Math.min(Math.max(value, min), max);
 };
