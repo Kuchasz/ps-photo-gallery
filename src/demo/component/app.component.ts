989a207c142bde105d6fe8509edaa444a996ad21
Kuchasz <kuchasz1@o2.pl>
2020-08-23 23:11:16 +0200

    more routing tweaks

--- src/demo/component/app.component.ts
+++ src/demo/component/app.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit, NgZone } from "@angular/core";
 import { GalleryService } from "../../index";
 import { Router, NavigationStart } from "@angular/router";
+import { first } from "rxjs/operators";
 
 @Component({
     selector: "app-root",
@@ -10,6 +11,14 @@ import { Router, NavigationStart } from "@angular/router";
 })
 export class AppComponent implements OnInit {
     constructor(public gallery: GalleryService, private router: Router) {
+        //for app start on directory/..../fullscreen
+        router.events.pipe(first()).subscribe((e) => {
+            if (e instanceof NavigationStart) {
+                if (/\/directory\/\w+\/{0}/g.test(e.url)) {
+                    this.gallery.selectDirectory(e.url.split("/")[2]);
+                }
+            }
+        });
         router.events.subscribe((e) => {
             if (e instanceof NavigationStart) {
                 if (/\/directory\/\w+\/{0}$/g.test(e.url)) {
