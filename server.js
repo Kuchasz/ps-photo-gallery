68f791c63cd5f695c535fd8124d426bb353616b8
Kuchasz <kuchasz1@o2.pl>
2020-09-01 21:17:23 +0200

    back to the server code

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, "dist")));
+app.use(express.static(path.resolve(__dirname, "client/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/dist", "index.html"));
 });
 
 app.listen(port);
